81749c51c1ec460cd0230f45d096d67718cc1ad1
DrACoder001 <DrACoder001@gmail.com>
2025-07-17 14:24:51 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -10,3 +10,13 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+let circleMarker01 = L.circle(coordinates, {
+  color: "black",
+  fillColor: "green",
+  fillOpacity: 0.5,
+  radius: 500
+}).bindPopup("<center>
+             <h3>Hello from the Maldives!</h3>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/W4YfDg-dKzk?si=LsBxWZXswQvPhopL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+             </center>").addTo(map);
