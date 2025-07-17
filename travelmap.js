e3cf637651c44c7c3c3f84d12fc64c5bebd8702b
DrACoder001 <DrACoder001@gmail.com>
2025-07-17 14:34:18 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -18,5 +18,5 @@ let circleMarker01 = L.circle(coordinates, {
   radius: 100
 }).bindPopup(`<center>
              <h3>Hello from the Maldives!</h3>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/W4YfDg-dKzk?si=LsBxWZXswQvPhopL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="200" height="100" src="https://www.youtube.com/embed/W4YfDg-dKzk?si=LsBxWZXswQvPhopL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
              </center>`).addTo(map);
